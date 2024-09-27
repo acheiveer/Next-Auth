875e519ca5aca4382b1c2af508a41c5eacd58d50
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-09-27 23:36:01 +0530

    Update

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+
+const handler = NextAuth({
+   providers:[
+      CredentialsProvider({
+         name: "Email",
+         credentials:{
+            username: { label: 'Username', type: 'text', placeholder: 'Email' },
+            password: { label: 'Password', type: 'password', placeholder: 'Password' },
+         },
+         async authorize(credentials: any) {
+            
+            return {
+                id: "user1"
+            };
+        },
+      })
+   ]
+})
+
+export const GET = handler;
+export const POST = handler;
